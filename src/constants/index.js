a9b1a04b667d541af8d5ccb25f743b9c2863675f
Saloni <talwar.sa@northeastern.edu>
2023-03-18 03:27:01 -0400

    Portfolio initial commit

---
+++ src/constants/index.js
@@ -0,0 +1,279 @@
+import {
+  mobile,
+  backend,
+  creator,
+  web,
+  javascript,
+  typescript,
+  html,
+  css,
+  reactjs,
+  redux,
+  tailwind,
+  nodejs,
+  mongodb,
+  git,
+  figma,
+  docker,
+  meta,
+  starbucks,
+  tesla,
+  deloitte,
+  shopify,
+  carrent,
+  ieltstar,
+  jobit,
+  portfolio,
+  tripguide,
+  threejs,
+} from "../assets";
+
+export const navLinks = [
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "work",
+    title: "Work",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+];
+
+const services = [
+  {
+    title: "Backend Developer",
+    icon: web,
+  },
+  {
+    title: "Frontend Developer",
+    icon: backend,
+  },
+  {
+    title: "Software Developer",
+    icon: creator,
+  },
+];
+
+const technologies = [
+  {
+    name: "HTML 5",
+    icon: html,
+  },
+  {
+    name: "CSS 3",
+    icon: css,
+  },
+  {
+    name: "JavaScript",
+    icon: javascript,
+  },
+  {
+    name: "TypeScript",
+    icon: typescript,
+  },
+  {
+    name: "React JS",
+    icon: reactjs,
+  },
+  {
+    name: "Redux Toolkit",
+    icon: redux,
+  },
+  {
+    name: "Tailwind CSS",
+    icon: tailwind,
+  },
+  {
+    name: "Node JS",
+    icon: nodejs,
+  },
+  {
+    name: "MongoDB",
+    icon: mongodb,
+  },
+  {
+    name: "Three JS",
+    icon: threejs,
+  },
+  {
+    name: "git",
+    icon: git,
+  },
+  // {
+  //   name: "figma",
+  //   icon: figma,
+  // },
+  // {
+  //   name: "docker",
+  //   icon: docker,
+  // },
+];
+
+const experiences = [
+  {
+    title: "Analyst",
+    company_name: "Deloitte US-India (USI)",
+    icon: deloitte,
+    iconBg: "#383E56",
+    date: "Aug 2021 - Aug 2022",
+    points: [
+      // "Developing and maintaining web applications using React.js and other related technologies.",
+      // "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+      // "Implementing responsive design and ensuring cross-browser compatibility.",
+      // "Participating in code reviews and providing constructive feedback to other developers.",
+      "Created e-commerce websites as backend developer for clients leveraging Salesforce Commerce Cloud Platform (SFCC) , cloud-based SaaS (Software as a Service)",
+      "Implemented frontend technical solutions to meet project requirement using AJAX, CSS, HTML",
+      "Generate APIs and Integrated 3rd party functionalities such as CyberSource, Apple pay, Salesforce Marketing Cloud, Salesforce Service Cloud, Google Analytics, Order Management System and Headless Commerce",
+      "Optimized internal functionalities, fixed regression defects",
+      "Computed reusable asset projects on GitHub and organized KT sessions",
+    ],
+  },
+  {
+    // title: "React Native Developer",
+    // company_name: "Tesla",
+    // icon: tesla,
+    // iconBg: "#E6DEDD",
+    // date: "Jan 2021 - Feb 2022",
+    // points: [
+    //   "Developing and maintaining web applications using React.js and other related technologies.",
+    //   "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+    //   "Implementing responsive design and ensuring cross-browser compatibility.",
+    //   "Participating in code reviews and providing constructive feedback to other developers.",
+    title: "Associate Analyst",
+    company_name: "Deloitte US-India (USI)",
+    icon: deloitte,
+    iconBg: "#E6DEDD",
+    date: "Aug 2019 - July 2021",
+    points: [
+      "Built e-commerce website as a Salesforce Commerce Cloud Backend Developer for clients deploying Agile Methodology",
+      "Designed, Developed, and Tested website features - home page, product list page, product display page, to order confirmation mail and product returns using OMS, SFMC and SFSC delivering seamless UI",
+      "Tracked JIRA status for internal team - customized tickets, updated status for tasks",
+      "Enforced Cross-platform assets and functionalities for clients",
+    ],
+  },
+  // {
+  //   title: "Web Developer",
+  //   company_name: "Shopify",
+  //   icon: shopify,
+  //   iconBg: "#383E56",
+  //   date: "Jan 2022 - Jan 2023",
+  //   points: [
+  //     "Developing and maintaining web applications using React.js and other related technologies.",
+  //     "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+  //     "Implementing responsive design and ensuring cross-browser compatibility.",
+  //     "Participating in code reviews and providing constructive feedback to other developers.",
+  //   ],
+  // },
+  // {
+  //   title: "Full stack Developer",
+  //   company_name: "Meta",
+  //   icon: meta,
+  //   iconBg: "#E6DEDD",
+  //   date: "Jan 2023 - Present",
+  //   points: [
+  //     "Developing and maintaining web applications using React.js and other related technologies.",
+  //     "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+  //     "Implementing responsive design and ensuring cross-browser compatibility.",
+  //     "Participating in code reviews and providing constructive feedback to other developers.",
+  //   ],
+  // },
+];
+
+const testimonials = [
+  {
+    testimonial:
+      "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
+    name: "Sara Lee",
+    designation: "CFO",
+    company: "Acme Co",
+    image: "https://randomuser.me/api/portraits/women/4.jpg",
+  },
+  {
+    testimonial:
+      "I've never met a web developer who truly cares about their clients' success like Rick does.",
+    name: "Chris Brown",
+    designation: "COO",
+    company: "DEF Corp",
+    image: "https://randomuser.me/api/portraits/men/5.jpg",
+  },
+  {
+    testimonial:
+      "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
+    name: "Lisa Wang",
+    designation: "CTO",
+    company: "456 Enterprises",
+    image: "https://randomuser.me/api/portraits/women/6.jpg",
+  },
+];
+
+const projects = [
+  {
+    name: "IELTSTAR",
+    description:
+      "Web-based platform that provide students to practice various sections of IELTS english proficiency exam",
+    tags: [
+      {
+        name: "next",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "express",
+        color: "green-text-gradient",
+      },
+      {
+        name: "mongodb",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: ieltstar,
+    source_code_link: "https://github.com/neu-mis-info6150-fall-2022/final-project-husky-bytes",
+  },
+  {
+    name: "Portfolio",
+    description:
+      "Web application to demonstrate my skillset and work experience",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "threejs",
+        color: "green-text-gradient",
+      },
+      {
+        name: "tailwind",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: portfolio,
+    source_code_link: "https://github.com/",
+  },
+  // {
+  //   name: "Trip Guide",
+  //   description:
+  //     "A comprehensive travel booking platform that allows users to book flights, hotels, and rental cars, and offers curated recommendations for popular destinations.",
+  //   tags: [
+  //     {
+  //       name: "nextjs",
+  //       color: "blue-text-gradient",
+  //     },
+  //     {
+  //       name: "supabase",
+  //       color: "green-text-gradient",
+  //     },
+  //     {
+  //       name: "css",
+  //       color: "pink-text-gradient",
+  //     },
+  //   ],
+  //   image: tripguide,
+  //   source_code_link: "https://github.com/",
+  // },
+];
+
+export { services, technologies, experiences, testimonials, projects };
